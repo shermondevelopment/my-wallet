03358f5fe918f584093622e0cb0b4bd5559d0a38
Guga <contato.gustavobarbosa@outlook.com>
2022-05-20 19:54:41 -0300

    Preparando revisão de código

--- app/utils/error-message.js
+++
@@ -1,7 +0,0 @@
-export default async function validation(data, schemaValidation) {
-  try {
-    const valid = await schemaValidation.validateAsync({...data})
-  } catch (error) {
-    return error.message
-  }
-}
--- app/utils/userChecking.js
+++
@@ -1,6 +0,0 @@
-/* model user */
-import user from '../schemas/user.js'
-
-export async function userChecking(email) {
-  return  await user.findOne({ email })
-}
